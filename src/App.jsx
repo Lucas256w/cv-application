e25c8eacb2d75840fff588905b6db584c129ad6d
Jiahao Huamani <lucashuamani256w@gmail.com>
2023-11-09 17:50:06 -0500

    Implement initial Work Experience edit section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import EducationEdit from "./components/EducationEdit";
+import WorkEdit from "./components/WorkEdit";
 
 function App() {
   const [educations, setEducations] = useState([]);
@@ -29,7 +30,7 @@ function App() {
       // If the form is already visible, hide it and clear the currentEducation
       setCurrentEducation({
         name: "",
-        position: "",
+        degree: "",
         "start date": "",
         "end date": "",
         location: "",
@@ -38,6 +39,23 @@ function App() {
       setEducationFormVisible(false);
     } else {
       setEducationFormVisible(true);
+    }
+  };
+
+  const toggleWorkForm = () => {
+    if (workFormVisible) {
+      // If the form is already visible, hide it and clear the currentEducation
+      setCurrentWork({
+        name: "",
+        position: "",
+        "start date": "",
+        "end date": "",
+        location: "",
+        editIndex: -1,
+      });
+      setWorkFormVisible(false);
+    } else {
+      setWorkFormVisible(true);
     }
   };
 
@@ -71,6 +89,38 @@ function App() {
     }
   };
 
+  const addOrEditWork = () => {
+    if (
+      currentWork.name &&
+      currentWork.position &&
+      currentWork["start date"] &&
+      currentWork["end date"] &&
+      currentWork.location
+    ) {
+      if (currentWork.editIndex === -1) {
+        // Add a new education
+        setWorks([...works, { ...currentWork }]);
+      } else {
+        // Edit an existing education
+        const updatedWorks = [...works];
+        updatedWorks[currentWork.editIndex] = {
+          ...currentWork,
+        };
+        setWorks(updatedWorks);
+      }
+      // Clear the currentEducation and hide the form
+      setCurrentWork({
+        name: "",
+        position: "",
+        "start date": "",
+        "end date": "",
+        location: "",
+        editIndex: -1,
+      });
+      setWorkFormVisible(false);
+    }
+  };
+
   const deleteEducations = (index) => {
     const updatedEducations = [...educations];
     updatedEducations.splice(index, 1);
@@ -80,6 +130,17 @@ function App() {
   const editEducations = (index) => {
     setCurrentEducation({ ...educations[index], editIndex: index });
     setEducationFormVisible(true);
+  };
+
+  const deleteWorks = (index) => {
+    const updatedWorks = [...works];
+    updatedWorks.splice(index, 1);
+    setWorks(updatedWorks);
+  };
+
+  const editWorks = (index) => {
+    setCurrentWork({ ...works[index], editIndex: index });
+    setWorkFormVisible(true);
   };
 
   const cancelEducationForm = () => {
@@ -95,22 +156,52 @@ function App() {
     setEducationFormVisible(false);
   };
 
+  const cancelWorkForm = () => {
+    // Clear the currentEducation and hide the form
+    setCurrentWork({
+      name: "",
+      degree: "",
+      "start date": "",
+      "end date": "",
+      location: "",
+      editIndex: -1,
+    });
+    setWorkFormVisible(false);
+  };
+
   const handleEducationChange = (event, what) => {
     setCurrentEducation({ ...currentEducation, [what]: event.target.value });
   };
 
+  const handleWorkChange = (event, what) => {
+    setCurrentWork({ ...currentWork, [what]: event.target.value });
+  };
+
   return (
-    <EducationEdit
-      educationFormVisible={educationFormVisible}
-      toggleEducationForm={toggleEducationForm}
-      currentEducation={currentEducation}
-      handleEducationChange={handleEducationChange}
-      addOrEditEducation={addOrEditEducation}
-      cancelEducationForm={cancelEducationForm}
-      educations={educations}
-      editEducations={editEducations}
-      deleteEducations={deleteEducations}
-    />
+    <div>
+      <EducationEdit
+        educationFormVisible={educationFormVisible}
+        toggleEducationForm={toggleEducationForm}
+        currentEducation={currentEducation}
+        handleEducationChange={handleEducationChange}
+        addOrEditEducation={addOrEditEducation}
+        cancelEducationForm={cancelEducationForm}
+        educations={educations}
+        editEducations={editEducations}
+        deleteEducations={deleteEducations}
+      />
+      <WorkEdit
+        workFormVisible={workFormVisible}
+        toggleWorkForm={toggleWorkForm}
+        currentWork={currentWork}
+        handleWorkChange={handleWorkChange}
+        addOrEditWork={addOrEditWork}
+        cancelWorkForm={cancelWorkForm}
+        works={works}
+        editWorks={editWorks}
+        deleteWorks={deleteWorks}
+      />
+    </div>
   );
 }
 
---
+++ src/components/WorkEdit.jsx
@@ -0,0 +1,68 @@
+const WorkEdit = ({
+  workFormVisible,
+  toggleWorkForm,
+  currentWork,
+  handleWorkChange,
+  addOrEditWork,
+  cancelWorkForm,
+  works,
+  editWorks,
+  deleteWorks,
+}) => {
+  return (
+    <div>
+      <h1>Work List</h1>
+      {!workFormVisible && (
+        <button onClick={toggleWorkForm}>Add New Work</button>
+      )}
+      {workFormVisible ? (
+        <div>
+          <input
+            type="text"
+            placeholder="Company Name"
+            value={currentWork.name}
+            onChange={(e) => handleWorkChange(e, "name")}
+          />
+          <input
+            type="text"
+            placeholder="Position"
+            value={currentWork.position}
+            onChange={(e) => handleWorkChange(e, "position")}
+          />
+          <input
+            type="text"
+            placeholder="Start Date"
+            value={currentWork["start date"]}
+            onChange={(e) => handleWorkChange(e, "start date")}
+          />
+          <input
+            type="text"
+            placeholder="End Date"
+            value={currentWork["end date"]}
+            onChange={(e) => handleWorkChange(e, "end date")}
+          />
+          <input
+            type="text"
+            placeholder="Location"
+            value={currentWork.location}
+            onChange={(e) => handleWorkChange(e, "location")}
+          />
+          <button onClick={addOrEditWork}>Submit</button>
+          <button onClick={cancelWorkForm}>Cancel</button>
+        </div>
+      ) : (
+        <ul>
+          {works.map((work, index) => (
+            <div key={index}>
+              {work.name}
+              <button onClick={() => editWorks(index)}>Edit</button>
+              <button onClick={() => deleteWorks(index)}>Delete</button>
+            </div>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default WorkEdit;
